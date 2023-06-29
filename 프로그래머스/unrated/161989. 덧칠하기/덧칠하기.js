b5a207ee1bd8450edbe932eb8d685e49e011a6ab
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2023-06-29 17:10:37 +0900

    [unrated] Title: 덧칠하기, Time: 4.12 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,14 @@
+function solution(n, m, section) {
+    let answer = 0
+    let arr = new Array(n).fill(true)
+    
+    section.forEach((element) => arr[element-1] = false)
+    
+    for(let i = 0 ; i < arr.length ; i++) {
+        if(!arr[i]) {
+            answer += 1
+            i += m-1
+        }    
+    }
+    return answer
+}
