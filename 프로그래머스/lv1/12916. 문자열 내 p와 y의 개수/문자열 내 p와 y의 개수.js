120ef26ecdebda0e55fdf7908b1a771c73069f40
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2023-07-24 11:23:34 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,10 +1,6 @@
 function solution(s){
-    var answer = '';
     let obj = {}
-    
-//     바로 드는 생각은 upper / lower 후 순회를 통해 객체 히스토그램 생성
-//     p key의 value와 y key의 value가 일치할 경우 참 반환
-    
     for(const el of s.toLowerCase()){obj[el]?obj[el]+=1:obj[el]=1}
+    
     return obj['p']===obj['y']?true:false
-}
+    }
