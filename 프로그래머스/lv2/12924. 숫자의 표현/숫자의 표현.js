4359269eed796bd66454cb48225cba2660d8efe2
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2023-07-31 11:04:07 +0900

    [level 2] Title: 숫자의 표현, Time: 1.42 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,24 @@
+function solution(n) {
+  let count = 0;
+  let sum = 0;
+  let left = 1;
+  
+  for (let right = 1; right <= n; right++) {
+    sum += right;
+    
+    while (sum > n) {
+      sum -= left;
+      left++;
+    }
+    
+    if (sum === n) {
+      count++;
+    }
+  }
+  
+  return count;
+}
+
+
+
+
