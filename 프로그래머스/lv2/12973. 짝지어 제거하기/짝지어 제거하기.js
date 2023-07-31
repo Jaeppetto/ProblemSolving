2a920d4ea3b385efc81a07b322afc1487f216e2f
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2023-07-31 17:49:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 37.75 ms, Memory: 58.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,9 @@
+function solution(s)
+{
+    let stack = []
+    for(const char of s){
+        if(stack[stack.length-1]===char) stack.pop()
+        else stack.push(char)
+    }
+    return stack.length===0?1:0
+}
