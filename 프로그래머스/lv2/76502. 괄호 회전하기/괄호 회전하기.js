984f0a7dff3dfa622f669eb33b0616b137113d91
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2023-08-03 19:00:33 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,53 @@
+function solution(s) {
+    var answer = 0;
+    let dst = s.split('')
+//     1. 한 글자씩 옮기는 알고리즘
+//     2. 옮긴 문자열이 올바른 괄호인지 확인하는 알고리즘
+    for(let i=0;i<s.length;i++){
+        if(i>=1){
+            const shift = dst.shift()
+            dst.push(shift)
+        }
+        isRight(dst)?answer+=1:answer+=0
+    }
+    return answer;
+}
+
+function seperate(arr){
+    let sm = [], md = [], lg = [];
+    let count = 0
+    
+    arr.forEach((el)=>{
+        if(el==='(' || el===')') sm.push(el)
+        else if(el==='{' || el==='}') md.push(el)
+        else if(el==='[' || el===']') lg.push(el)
+    })
+    
+    const isSmRight = isRight(sm)
+    const isMdRight = isRight(md)
+    const isLgRight = isRight(lg)
+    
+    return (isSmRight===true && isMdRight===true && isLgRight===true)?true:false
+}
+
+function isRight(arr){
+    const stack = [];
+
+  for (let i=0;i<arr.length;i++) {
+      const char = arr[i]
+    if (char === '(' || char === '{' || char === '[') {
+      stack.push(char);
+    } else {
+      const lastOpenBracket = stack.pop();
+      if (
+        (char === ')' && lastOpenBracket !== '(') ||
+        (char === '}' && lastOpenBracket !== '{') ||
+        (char === ']' && lastOpenBracket !== '[')
+      ) {
+        return 0; // 올바른 괄호 문자열이 아니면 바로 0을 반환하고 종료
+      }
+    }
+  }
+
+  return stack.length === 0 ? true : false;
+}
