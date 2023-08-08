1f3f29f01d008e871466a43f287c6bc3ee6eaca2
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2023-08-09 01:20:32 +0900

    [level 2] Title: 오픈채팅방, Time: 163.56 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,21 @@
+function solution(record) {
+    const answer = [];
+    const idPerUserName = {}
+    record.forEach((data)=>{
+        const [status,uid,target] = data.split(' ')
+        if(status === 'Enter' || status === 'Change')
+            idPerUserName[uid] = target
+    })
+
+    record.forEach((data)=>{
+        const [status,uid,target] = data.split(' ')
+        if(status === 'Enter'){
+            answer.push(`${idPerUserName[uid]}님이 들어왔습니다.`)
+        }
+        if(status === 'Leave'){
+            answer.push(`${idPerUserName[uid]}님이 나갔습니다.`)
+        }
+    })
+
+    return answer;
+}
