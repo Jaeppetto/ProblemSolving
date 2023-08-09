ca953fd68e12bb5ecbbc1511a4d35faa1729ca11
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2023-08-09 17:35:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 28.08 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,31 @@
+function solution(maps) {
+    let answer = -1;
+    
+    const dx = [0,0,1,-1]
+    const dy = [1,-1,0,0]    
+    const height = maps.length
+    const width = maps[0].length
+    
+    const stack = [{coorY:0,coorX:0,count:1}]
+
+    while(stack.length !== 0){
+        const {coorY,coorX,count} = stack.pop()
+
+        for(let i=0;i<4;i++){
+            const newCoorY = coorY + dx[i]
+            const newCoorX = coorX + dy[i]
+
+            if(coorY === height-1 && coorX === width - 1){
+                answer = count
+                return answer
+            }
+
+            if(newCoorY >=0 && newCoorY < height && newCoorX >=0 && newCoorX < width && maps[newCoorY][newCoorX] === 1){
+                maps[newCoorY][newCoorX] = 0
+                stack.unshift({coorY:newCoorY, coorX:newCoorX, count : count+1})
+            }
+        }
+    }
+
+    return answer;
+}
