4538393ce1ea63f384a2a08f251350688dfefc49
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2023-08-11 01:23:07 +0900

    [unrated] Title: 숫자 변환하기, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,24 @@
+function solution(x, y, n) {
+    var answer = -1;
+    const queue = [{num:y,count:0}]
+    
+    while(queue.length>0){
+        const {num,count} = queue.shift()
+        
+        if(num===x){
+            answer = count
+            break;
+        }
+        
+        if(num%2===0){
+            queue.push({num:num/2,count:count+1})
+        }
+        if(num%3===0){
+            queue.push({num:num/3,count:count+1})
+        }
+        if(num-n>=x){
+            queue.push({num:num-n,count:count+1})
+        }
+    }
+    return answer;
+}
