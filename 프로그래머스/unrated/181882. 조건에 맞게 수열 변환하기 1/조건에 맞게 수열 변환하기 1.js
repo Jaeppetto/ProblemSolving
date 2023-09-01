bc94428683b6c59cd79786148c93c40b0e5a2d5f
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2023-09-01 16:57:45 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 3.82 ms, Memory: 43.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    var answer = [];
+    return arr.map(el=>{
+        if(el>=50 && el%2===0){
+            return el/2
+        } else if (el<50 && el%2===1){
+            return el*2
+        } else{
+            return el
+        }});
+}
