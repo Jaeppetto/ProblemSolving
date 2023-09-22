5ace77900fc352f25b8518c1ca41cceec7a6c098
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2023-09-22 16:03:40 +0900

    [level 2] Title: 전화번호 목록, Time: 313.41 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,18 +1,15 @@
-function solution(phone_book) {
-    let answer = true
-    phone_book.sort()
-    for(let i=0;i<phone_book.length-1;i++){
-        const current = phone_book[i]
-        const strLength = current.length
-        const nextSlice = phone_book[i+1].substr(0,strLength)
-        
-        if(current===nextSlice) return false
-    }
-    console.log(phone_book)
-    return answer;
-}
+function solution(phoneBook) {
+    const map = new Map()
+    for (const number of phoneBook) {
+    map.set(number,true)
+  };
+    
+    for (const number of phoneBook) {
+        for (let i = 1; i < number.length; i += 1) {
+            const prefix = number.slice(0, i);
+            if (map.has(prefix)) return false;  
+        };
+  };
 
-// 일반적으로 생각하면, 배열 순회하며 2단 루프로 비교해보면 됨
-// 해시는 무조건 맵과 함께 사용?
-// 인덱스를 key로, 전화번호를 value로 갖는 맵 구조 생성
-// 배열 순회하면서 현재 요소의 길이
+    return true
+}
