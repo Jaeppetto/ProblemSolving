46703de26569703e44e7b6de693ea4c629affbac
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2023-10-02 15:39:21 +0900

    [level 2] Title: 타겟 넘버, Time: 11.06 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,15 +1,24 @@
 function solution(numbers, target) {
-    var answer = 0;
+    let count = 0
+    const stack = [[numbers[0],0],[-numbers[0], 0]]
     
-    function DFS(index,currentSum){
-        if(index === numbers.length){
-            if(currentSum === target) answer+=1
-        return
+    while(stack.length>0){
+        let [currentValue,currentIdx] = stack.pop()
+        const nextIdx = currentIdx+1
+        
+        if(currentIdx===numbers.length-1 && currentValue===target){
+            count++
+        }else if(currentIdx<numbers.length-1){
+            stack.push([currentValue+numbers[nextIdx],currentIdx+1])
+            stack.push([currentValue-numbers[nextIdx],currentIdx+1])
         }
-        DFS(index+1,currentSum + numbers[index])
-        DFS(index+1,currentSum - numbers[index])
     }
-    DFS(0,0)
-    
-    return answer;
+    return count
 }
+
+// 모든 경우의 수를 탐색해야 하니 백트래킹이라고도 볼 수 있나? 근데 가지치기는 어떻게 할지
+// 재귀를 이용하면 그리 어렵지 않게 구할 수 있을 것 같은데, 스택으로는 어떻게?
+
+// 맨처음 하나 넣어, 빼고 다음 원소 +-한 것 두 개 넣어
+// 스택에서 뺀 원소 길이가 numbers의 length와 같을 때, 그 계산한 값이 target이면 count++
+// [계산한값,현재인덱스] -> 현재 인덱스가 길이랑 같을 경우 비교연산
