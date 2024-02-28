60c209053dcda2580df201cc20ca3845fd1452d1
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2024-02-28 15:03:34 +0900

    [Silver IV] Title: 카드2, Time: 416 ms, Memory: 89060 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      // 리스트가 비어있는 경우
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+
+    this.tail = newNode;
+    this.length++;
+
+    return newNode;
+  }
+
+  shift() {
+    this.head = this.head.next;
+    this.head.prev = null;
+    this.length--;
+  }
+
+  getHead() {
+    return this.head.value;
+  }
+
+  getSize() {
+    return this.length;
+  }
+}
+
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim();
+
+const list = new LinkedList();
+for (let i = 1; i <= input; i++) {
+  list.push(i);
+}
+
+while (list.length > 1) {
+  list.shift();
+  list.push(list.getHead());
+  list.shift();
+}
+
+console.log(list.getHead());
